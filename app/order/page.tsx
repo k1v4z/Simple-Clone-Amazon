a852d24250d6299ac9d958f3c5678ee043176625
k1v4z <addnguyen32gmail.com>
2024-06-27 03:10:58 +0700

    add order route

---
+++ app/order/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+import OrderContent from '../components/Order/OrderContent'
+
+const OrderPage = () => {
+  return (
+    <div>
+      <Header/>
+      <OrderContent/>
+    </div>
+  )
+}
+
+export default OrderPage
