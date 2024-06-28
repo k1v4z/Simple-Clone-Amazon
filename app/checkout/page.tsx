523ca894c4fb40439ba6a37b070dbb567aadd082
k1v4z <addnguyen32gmail.com>
2024-06-29 06:04:22 +0700

    define checkout route

---
+++ app/checkout/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CheckoutHeader from '../components/Header/CheckoutHeader'
+import CheckoutList from '../components/Checkout/CheckoutList'
+
+const CheckoutPage = () => {
+  return (
+    <div>
+      <CheckoutHeader/>
+      <CheckoutList/>
+    </div>
+  )
+}
+
+export default CheckoutPage
