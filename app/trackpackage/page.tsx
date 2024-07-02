8f128fa0ed175492b10a9db96d05df3195cba6b5
k1v4z <addnguyen32gmail.com>
2024-07-03 06:51:25 +0700

    init trackpackge route

---
+++ app/trackpackage/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+import Tracking from '../components/TrackPackage/Tracking'
+
+const TrackPackage = () => {
+  
+  return (
+    <div>
+      <Header/>
+      <Tracking/>
+    </div>
+  )
+}
+
+export default TrackPackage
